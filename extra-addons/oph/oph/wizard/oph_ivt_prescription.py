76fb6918f541812a2571bb0f649df27022e07ba6
Laurent FRANCOIS <francois.oph@gmail.com>
2018-05-22 23:10:58 +1100

    adding a pdf report for ivt prescription

--- extra-addons/oph/oph/wizard/oph_ivt_prescription.py
+++ extra-addons/oph/oph/wizard/oph_ivt_prescription.py
@@ -9,17 +9,17 @@ class oph_ivt_prescription(osv.osv_memory):
 
     _columns = {
               # 'name': fields.char('Molecule', size = 128, required = True),
-              'date': fields.date('Statement Date', required = True, help = "Date of the current day to print on report"),
+              'date': fields.date('Statement Date', required=True, help="Date of the current day to print on report"),
               # 'molecule':fields.char('Molecule', size = 64),
-              'molecule':fields.many2one('oph.brandname', 'Brandname', domain = "[('ivt','=',True)]"),
-              'indication':fields.many2one('oph.indication', 'Indication', domain = "[('ivt','=',True)]"),
+              'molecule':fields.many2one('oph.brandname', 'Brandname', domain="[('ivt','=',True)]"),
+              'indication':fields.many2one('oph.indication', 'Indication', domain="[('ivt','=',True)]"),
               }
 
     _defaults = {
                'date': lambda *a: time.strftime('%Y-%m-%d'),
                }
 
-    def print_ivt_prescription(self, cr, uid, ids, context = None):
+    def print_ivt_prescription(self, cr, uid, ids, context=None):
         if context is None:
             context = {}
 
@@ -28,7 +28,7 @@ class oph_ivt_prescription(osv.osv_memory):
         if not active_ids:
             return {'type': 'ir.actions.act_window_close'}
 
-        data = self.read(cr, uid, ids, context = context)[0]
+        data = self.read(cr, uid, ids, context=context)[0]
 
         context['date_report'] = data.get('date')
         context['molecule'] = data.get('molecule', '')[1]
@@ -45,6 +45,33 @@ class oph_ivt_prescription(osv.osv_memory):
         return {
             'type': 'ir.actions.report.xml',
             'report_name':'oph.ivt.prescription',
+            'datas':datas,
+            'context':context
+            }
+        
+    def print_ivt_prescription_pdf(self, cr, uid, ids, context=None):
+        if context is None:
+            context = {}
+
+        active_ids = context.get('active_ids')
+        if not active_ids:
+            return {'type': 'ir.actions.act_window_close'}
+
+        data = self.read(cr, uid, ids, context=context)[0]
+
+        context['date_report'] = data.get('date')
+        context['molecule'] = data.get('molecule', '')[1]
+        context['indication'] = data.get('indication', '')[1]
+        datas = {
+           'ids':active_ids,
+           'model':'oph.bloc.agenda.line',
+           'form':data,
+           'context':context,
+           }
+
+        return {
+            'type': 'ir.actions.report.xml',
+            'report_name':'oph.ivt.prescription.pdf',
             'datas':datas,
             'context':context
             }
